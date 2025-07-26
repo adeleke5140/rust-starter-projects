8bc5006f4384677b69713d290adc837539a29f1a
adeleke5140 <adelekekehinde06@gmail.com>
2025-07-26 13:56:21 +0100

    feat: simplify fibonacci by removing hash map

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,11 +1,6 @@
-use std::collections::HashMap;
 use std::io;
 
 fn main() {
-    let mut f: HashMap<u32, u32> = HashMap::new();
-    f.insert(0, 0);
-    f.insert(1, 1);
-
     loop {
         println!("Enter the number to get its fibonacci value");
 
@@ -19,22 +14,21 @@ fn main() {
             Err(error) => panic!("Problem parsing number: {error:?}"),
         };
 
-        let res = find_fibonacci_sequence(num, &mut f);
+        let res = find_fibonacci_sequence(num);
         println!("the {}th fibonacci number is {}", num, res);
     }
 }
 
-fn find_fibonacci_sequence(n: u32, f: &mut HashMap<u32, u32>) -> u32 {
+fn find_fibonacci_sequence(n: u32) -> u32 {
     let mut sum = 0;
     if n == 0 {
-        sum = f[&0];
+        sum += 0;
         return sum;
     } else if n == 1 {
-        sum = f[&1];
+        sum+=1;
         return sum;
     } else {
-        sum = find_fibonacci_sequence(n - 1, f) + find_fibonacci_sequence(n - 2, f);
-        f.insert(n, sum);
+        sum = find_fibonacci_sequence(n - 1) + find_fibonacci_sequence(n - 2);
         return sum;
     }
 }
